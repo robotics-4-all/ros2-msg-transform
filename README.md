e14240ddbc9a677f39bf2da1fc63b5cd62be8424
Konstantinos Panayiotou <klpanagi@gmail.com>
2021-01-02 19:56:34 +0200

    docs

--- README.md
+++ README.md
@@ -4,4 +4,91 @@ Implements ros2 message conversion operations.
 
 ## Methods
 
-### ros2_msg_to_dict(msg)
+```
+dict_to_ros2_msg_from_ns(py_dict: OrderedDict, msg_type: str) -> Any
+```
+
+### Convert ROS2 structures to OrderedDict
+
+Can be used to convert any type of ROS2 communication structure
+(Topic Message, Service, Action).
+
+```python
+def ros2_msg_to_dict(msg: Any) -> OrderedDict:
+    """ros2_msg_to_dict.
+    Converts a ROS2 communication message to an OrderedDict.
+
+    Args:
+        msg: ROS2 Message to transform. Can be one of type:
+            - Topic Message (Message)
+            - Service Request Message (Service.Request)
+            - Service Response Message (Service.Response)
+            - Action Goal Message (Action.Goal)
+            - Action Cancel Message (Action.Cancel)
+            - Action Result Message (Action.Result)
+            - Action Status Message (Action.Status)
+            - Action Feedback Message (Action.Feedback)
+    """
+```
+
+Below are two examples, one for converting a ROS2 msg and one for converting
+a ROS2 srv into an OrderedDict.
+
+```python
+from sensor_msgs.msg import Range
+
+msg = Range()
+_dict = ros2_msg_to_dict(msg)
+```
+
+```python
+from std_srvs.srv import SetBool
+
+req = SetBool.Request()
+_dict = ros2_msg_to_dict(req)
+```
+
+### Convert an OrderedDict to a ROS2 structure
+
+Can be used to convert into any type of ROS2 communication structure
+(Topic Message, Service, Action).
+
+```python
+def dict_to_ros2_msg(py_dict: OrderedDict, msg_cls: Any) -> Any:
+    """dict_to_ros2_msg.
+    Converts a dict/OrderedDict into a ROS2 message, given the message cls
+        as input.
+
+    Args:
+        py_dict (OrderedDict): Dict/OrderedDict to convert to ROS2 message.
+        msg_cls (Any): ROS2 Message class.
+    """
+    m = msg_cls()
+    set_message_fields(m, py_dict)
+    return m
+```
+
+Below are two examples, one for converting a ROS2 msg and one for converting
+a ROS2 srv into an OrderedDict.
+
+```python
+from sensor_msgs.msg import Range
+
+_dict = {
+    'range': 10.0,
+    'min_range': 0.2,
+    'max_range': 240.0,
+    'field_of_view': 60.0,
+    'radiation_type': 0,
+}
+_msg = dict_to_ros2_msg(_dict, Range)
+```
+
+```python
+from std_srvs.srv import SetBool
+
+...
+
+_req = dict_to_ros2_msg(_dict, SetBool.Request)
+_resp = dict_to_ros2_msg(_dict, SetBool.Response)
+```
--- ros2_msg_conv/ros2_conv.py
+++ ros2_msg_conv/ros2_conv.py
@@ -9,6 +9,7 @@ from rosidl_parser.definition import NamespacedType
 
 def ros2_msg_to_dict(msg: Any) -> OrderedDict:
     """ros2_msg_to_dict.
+    Converts a ROS2 communication message to an OrderedDict.
 
     Args:
         msg: ROS2 Message to transform. Can be one of type:
@@ -26,12 +27,28 @@ def ros2_msg_to_dict(msg: Any) -> OrderedDict:
 
 
 def dict_to_ros2_msg(py_dict: OrderedDict, msg_cls: Any) -> Any:
+    """dict_to_ros2_msg.
+    Converts a dict/OrderedDict into a ROS2 message, given the message cls
+        as input.
+
+    Args:
+        py_dict (OrderedDict): Dict/OrderedDict to convert to ROS2 message.
+        msg_cls (Any): ROS2 Message class.
+    """
     m = msg_cls()
     set_message_fields(m, py_dict)
     return m
 
 
 def dict_to_ros2_msg_from_ns(py_dict: OrderedDict, msg_type: str) -> Any:
+    """dict_to_ros2_msg_from_ns.
+    Converts a dict/OrderedDict into a ROS message, given the message type
+    in string representation (e.g. 'sensor_msgs/Range').
+
+    Args:
+        py_dict (OrderedDict): Dict/OrderedDict to convert to ROS2 message.
+        msg_type (str): ROS2 Message type.
+    """
     (pkg, name) = msg_type.split('/')
     msg_ns_type = NamespacedType([pkg, 'msg'], name)
     msg_cls = import_message_from_namespaced_type(msg_ns_type)
@@ -39,7 +56,39 @@ def dict_to_ros2_msg_from_ns(py_dict: OrderedDict, msg_type: str) -> Any:
     return dict_to_ros2_msg(py_dict, msg_cls)
 
 
-def dict_to_ros2_srv_from_ns(py_dict, srv_type):
+def dict_to_ros2_srv_req(py_dict: OrderedDict, srv_cls: Any) -> Any:
+    """dict_to_ros2_srv_req.
+    Converts a dict/OrderedDict into a ROS2 Service request,
+        given the svc cls as input.
+
+    Args:
+        py_dict (OrderedDict): Dict/OrderedDict to convert to ROS2 message.
+        srv_cls (Any): ROS2 Message class.
+    """
+    return dict_to_ros2_msg(py_dict, srv_cls.Request)
+
+
+def dict_to_ros2_srv_resp(py_dict: OrderedDict, srv_cls: Any) -> Any:
+    """dict_to_ros2_srv_resp.
+    Converts a dict/OrderedDict into a ROS2 Service response,
+        given the svc cls as input.
+
+    Args:
+        py_dict (OrderedDict): Dict/OrderedDict to convert to ROS2 message.
+        srv_cls (Any): ROS2 Message class.
+    """
+    return dict_to_ros2_msg(py_dict, srv_cls.Response)
+
+
+def dict_to_ros2_srv_from_ns(py_dict, srv_type) -> Any:
+    """dict_to_ros2_srv_from_ns.
+    Converts a dict/OrderedDict into a ROS message, given the message type
+    in string representation (e.g. 'std_srvs/SetBool.Request').
+
+    Args:
+        py_dict (OrderedDict): Dict/OrderedDict to convert to ROS2 message.
+        srv_type (str): ROS2 Message type.
+    """
     if '.' not in srv_type:
         raise ValueError('Not a valid srv_type given.')
     (pkg, name) = srv_type.split('/')
