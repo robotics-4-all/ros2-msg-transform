705f6e67b108d5904593b305f6c97e191f2741b8
Konstantinos Panayiotou <klpanagi@gmail.com>
2021-01-02 17:27:56 +0200

    .

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ros2_msg_conversions
 Implements ros2 message conversion operations.
+
+
+## Methods
+
+### ros2_msg_to_dict(msg)
--- ros2_msg_conv/ros2_conv.py
+++ ros2_msg_conv/ros2_conv.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from typing import Any
 
 from rosidl_runtime_py.convert import message_to_ordereddict
 from rosidl_runtime_py.import_message import import_message_from_namespaced_type
@@ -6,18 +7,31 @@ from rosidl_runtime_py.set_message import set_message_fields
 from rosidl_parser.definition import NamespacedType
 
 
-def ros2_msg_to_dict(msg):
+def ros2_msg_to_dict(msg: Any) -> OrderedDict:
+    """ros2_msg_to_dict.
+
+    Args:
+        msg: ROS2 Message to transform. Can be one of type:
+            - Topic Message (Message)
+            - RPC Request Message (Service.Request)
+            - RPC Response Message (Service.Response)
+            - Action Goal Message (Action.Goal)
+            - Action Cancel Message (Action.Cancel)
+            - Action Result Message (Action.Result)
+            - Action Status Message (Action.Status)
+            - Action Feedback Message (Action.Feedback)
+    """
     _d = message_to_ordereddict(msg)
     return _d
 
 
-def dict_to_ros2_msg(py_dict, msg_cls):
+def dict_to_ros2_msg(py_dict: OrderedDict, msg_cls: Any) -> Any:
     m = msg_cls()
     set_message_fields(m, py_dict)
     return m
 
 
-def dict_to_ros2_msg_from_ns(py_dict, msg_type):
+def dict_to_ros2_msg_from_ns(py_dict: OrderedDict, msg_type: str) -> Any:
     (pkg, name) = msg_type.split('/')
     msg_ns_type = NamespacedType([pkg, 'msg'], name)
     msg_cls = import_message_from_namespaced_type(msg_ns_type)
